18fe068916f23b318117f6fa4c694e68340c7d73
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-03-28 23:21:26 +0100

    update todo-cli to 1.4.6

--- Formula/todo-cli.rb
+++ Formula/todo-cli.rb
@@ -1,17 +1,17 @@
 class TodoCli < Formula
   desc "To-Do list CLI application written in Go"
   homepage "https://github.com/wim-vdw/todo-cli"
-  version "1.4.5"
+  version "1.4.6"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/wim-vdw/todo-cli/releases/download/v1.4.5/todo-cli_1.4.5_darwin_arm64.tar.gz"
-    sha256 "fda2201c4ad2da28e4328f45d26d4c854c6035c7ef480257ccac8367fb0b70f0"
+    url "https://github.com/wim-vdw/todo-cli/releases/download/v1.4.6/todo-cli_1.4.6_darwin_arm64.tar.gz"
+    sha256 "5c77f1cdd465df470a88517b493c1209b310c968c3256cfc463ff0b088cd7824"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/wim-vdw/todo-cli/releases/download/v1.4.5/todo-cli_1.4.5_darwin_amd64.tar.gz"
-    sha256 "ac6686aae0053517be3c4479a0f4834a4e313181ffdfbf3c09654aef175d8fbb"
+    url "https://github.com/wim-vdw/todo-cli/releases/download/v1.4.6/todo-cli_1.4.6_darwin_amd64.tar.gz"
+    sha256 "148dffabaff48bbffa91eecf9332f2fca68ab864b307e186f8d9bb0e22f9e065"
   end
 
   def install
