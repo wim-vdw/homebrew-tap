59ae1a6d3a4fa60485ad0b42b7270b97c55fe14c
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2026-02-16 14:58:25 +0100

    update wimkube to version 1.1.3 with new URLs and checksums

--- Formula/wimkube.rb
+++ Formula/wimkube.rb
@@ -1,17 +1,17 @@
 class Wimkube < Formula
   desc "Interactive Kubernetes CLI"
   homepage "https://github.com/wim-vdw/wimkube"
-  version "1.1.1"
+  version "1.1.3"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/wim-vdw/wimkube/releases/download/v1.1.1/wimkube-1.1.1-darwin-arm64.tar.gz"
-    sha256 "13bc001e79051a759865784ca397f005c5380e2b488aadd1db9dd5fee348afe2"
+    url "https://github.com/wim-vdw/wimkube/releases/download/v1.1.3/wimkube-1.1.3-darwin-arm64.tar.gz"
+    sha256 "60668c556753f9c42cb406130854338cb7f223d47e0f8e07c7405d83775c7b84"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/wim-vdw/wimkube/releases/download/v1.1.1/wimkube-1.1.1-darwin-amd64.tar.gz"
-    sha256 "08a22b3f538c95f2ddd3ecdab1094d25e8440e662c021420f48f5f44390ce411"
+    url "https://github.com/wim-vdw/wimkube/releases/download/v1.1.3/wimkube-1.1.3-darwin-amd64.tar.gz"
+    sha256 "9b24468f8ea2df7b4cc912bc5eb194363b99eed6976b67b83314f7f5dcbfd638"
   end
 
   def install
