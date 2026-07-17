795411110b3374d4693ec0fc4385fd437fdeed01
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2026-07-17 22:42:24 +0200

    update az-secret2env to version 1.1.4 with new URLs and checksums

--- Formula/az-secret2env.rb
+++ Formula/az-secret2env.rb
@@ -1,17 +1,17 @@
 class AzSecret2env < Formula
   desc "Execute a program with environment variables temporarily populated by Azure Key Vault secrets"
   homepage "https://github.com/wim-vdw/az-secret2env"
-  version "1.1.3"
+  version "1.1.4"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/wim-vdw/az-secret2env/releases/download/v1.1.3/az-secret2env-1.1.3-darwin-arm64.tar.gz"
-    sha256 "65c839ea791f75bf74ba43fa9af4ab4eeef2bd5178a3be200ad0b5c34ab673bb"
+    url "https://github.com/wim-vdw/az-secret2env/releases/download/v1.1.4/az-secret2env-1.1.4-darwin-arm64.tar.gz"
+    sha256 "634873492053c38d74c7fd4dffb03d2427f329185bd821e4b7ad40fc4857f2cd"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/wim-vdw/az-secret2env/releases/download/v1.1.3/az-secret2env-1.1.3-darwin-amd64.tar.gz"
-    sha256 "3bb37fbbabf6f352f19da85b3ef62e8591a5825c3041e9322df5dd4e209d3b19"
+    url "https://github.com/wim-vdw/az-secret2env/releases/download/v1.1.4/az-secret2env-1.1.4-darwin-amd64.tar.gz"
+    sha256 "719c21a24f45e22e94d249e050cf0b065883bd019a019c3b2322a49994aa0def"
   end
 
   def install
